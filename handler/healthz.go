3ec75dae085f63286e4a3c7658cb097ed977e46a
taku10101 <taku1011ht@gmail.com>
2023-12-22 06:12:51 -0800

    task 8 done

--- handler/healthz.go
+++ handler/healthz.go
@@ -8,19 +8,22 @@ import (
 	"github.com/TechBowl-japan/go-stations/model"
 )
 
-// A HealthzHandler implements health check endpoint.
+// ここで、HealthzHandler構造体を定義している
 type HealthzHandler struct{}
 
-// NewHealthzHandler returns HealthzHandler based http.Handler.
+// ここで、NewHealthzHandler関数を定義している
 func NewHealthzHandler() *HealthzHandler {
 	return &HealthzHandler{}
 }
 
-// ServeHTTP implements http.Handler interface.
+// w http.ResponseWriterは、HTTPレスポンスを書き込むための構造体
+// r *http.Requestは、HTTPリクエストを表す構造体
+// ServeHTTPメソッドは、HTTPリクエストを受け取り、HTTPレスポンスを返す
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	res := &model.HealthzResponse{
 		Message: "OK",
 	}
+	//json.NewEncoder(w)は、HTTPレスポンスを書き込むための構造体を引数に取り、JSONエンコーダを返す
 	err := json.NewEncoder(w).Encode(res)
 	if err != nil {
 		log.Println(err)
--- service/todo.go
+++ service/todo.go
@@ -23,19 +23,34 @@ func NewTODOService(db *sql.DB) *TODOService {
 func (s *TODOService) CreateTODO(ctx context.Context, subject, description string) (*model.TODO, error) {
 	const (
 		insert  = `INSERT INTO todos(subject, description) VALUES(?, ?)`
-		confirm = `SELECT subject, description, created_at, updated_at FROM todos WHERE id = ?`
+		confirm = `SELECT id, subject, description, created_at, updated_at FROM todos WHERE id = ?`
 	)
+	//ExecContext は、SQLを実行する。
+	//第一引数には、コンテキストを渡す。第二引数には、SQLを渡す。第三引数以降には、SQLに渡すパラメータを渡す
 	result, err := s.db.ExecContext(ctx, insert, subject, description)
+	if err != nil {
+		//ここでエラーが発生した場合は、エラーを返す
+		return nil, err
+	}
+	//LastInsertIdメソッドは、直近のINSERTで生成されたIDを返す
+	id, err := result.LastInsertId()
+	//ここでエラーが発生した場合は、エラーを返す
 	if err != nil {
 		return nil, err
 	}
 
-	lastId, err := s.db.ExecContext(ctx, insert, subject, description)
-	if lastId != nil {
-		return lastId
+	//QueryRowContextメソッドは、SQLを実行し、その結果を1行だけ取得する
+	row := s.db.QueryRowContext(ctx, confirm, id)
+	//Scanメソッドは、引数に渡した変数に対して、データベースから取得した値を格納する
+	todo := &model.TODO{}
+	//errには、row.Scanメソッドの実行結果が格納される
+	err = row.Scan(&todo.ID, &todo.Subject, &todo.Description, &todo.CreatedAt, &todo.UpdatedAt)
+	//実行結果がエラーだった場合は、エラーを返す
+	if err != nil {
+		return nil, err
 	}
-
-	return nil, nil
+	//エラーが発生しなかった場合は、todoを返す
+	return todo, nil
 }
 
 // ReadTODO reads TODOs on DB.
