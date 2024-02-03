95357a71c1873210e5d46694a0176a848c83904a
taku10101 <taku1011ht@gmail.com>
2024-02-04 01:47:59 +0900

    router add

--- handler/router/router.go
+++ handler/router/router.go
@@ -2,16 +2,64 @@ package router
 
 import (
 	"database/sql"
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/handler"
+	"github.com/TechBowl-japan/go-stations/service"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	// register routes
 	mux := http.NewServeMux()
-
-	mux.HandleFunc("/healthz", handler.NewHealthzHandler().ServeHTTP)
-
+	// healthzに関するルータを定義
+	healthRouter(mux)
+	todoRouter(mux, todoDB)
 	return mux
+}
+
+// healthzに関するルータを定義
+func healthRouter(mux *http.ServeMux) {
+	healthz := handler.NewHealthzHandler()
+	mux.HandleFunc("/healthz", healthz.ServeHTTP)
+
+}
+
+// todoに関するルータを定義
+func todoRouter(mux *http.ServeMux, db *sql.DB) {
+	todo := handler.NewTODOHandler(service.NewTODOService(db))//TODOHandlerを生成
+
+	mux.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {//"/todos"に対するリクエストを処理する
+		var err error
+		switch r.Method {
+		case http.MethodGet:
+			//w は、HTTPレスポンスを書き込むための構造体
+			//r は、HTTPリクエストを表す構造体
+			err = responseJson(todo.ReadTodo(w, r))//ReadTodoを呼び出し、結果をJSONで返す
+		case http.MethodPost:
+			err = responseJson(todo.CreateTodo(w, r))
+		case http.MethodPut:
+			err = responseJson(todo.UpdateTodo(w, r))
+		case http.MethodDelete:
+			err = responseJson(todo.DeleteTodo(w, r))
+		default:
+			// TODO:エラーハンドリングする
+		}
+		//エラーがあった場合ログ出力して500を返す
+		if err != nil {
+			log.Println(err)
+			responseJson(w, http.StatusInternalServerError, err)
+		}
+	})
+}
+
+// 任意のstatusをヘッドに入れたレスポンスを返す
+func responseJson(w http.ResponseWriter, status int, response interface{}) error {
+	
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		return err
+	}
+	return nil
 }
