b385f95657fd119753b4cde1422021169187e4a7
taku10101 <taku1011ht@gmail.com>
2024-02-04 01:37:06 +0900

    model fix

--- model/todo.go
+++ model/todo.go
@@ -3,38 +3,48 @@ package model
 import "time"
 
 type (
-	// A TODO expresses ...
-	TODO struct {
-		ID          int       `json:"id"`
-		Subject     string    `json:"subject"`
-		Description string    `json:"description"`
-		CreatedAt   time.Time `json:"created_at"`
-		UpdatedAt   time.Time `json:"updateed_at"`
-	}
-
-	// A CreateTODORequest expresses ...
-	CreateTODORequest struct {
-		Subject     string `json:"subject"`
-		Description string `json:"description"`
-	}
 	// A CreateTODOResponse expresses ...
 	CreateTODOResponse struct {
 		TODO `json:"todo"`
 	}
-
-	// A ReadTODORequest expresses ...
-	ReadTODORequest struct {
-	}
 	// A ReadTODOResponse expresses ...
-	ReadTODOResponse struct{}
+	ReadTODOResponse struct {
+		TODOs []*TODO `json:"todos"`
+	}
 
-	// A UpdateTODORequest expresses ...
-	UpdateTODORequest struct{}
 	// A UpdateTODOResponse expresses ...
-	UpdateTODOResponse struct{}
+	UpdateTODOResponse struct {
+		TODO `json:"todo"`
+	}
 
-	// A DeleteTODORequest expresses ...
-	DeleteTODORequest struct{}
 	// A DeleteTODOResponse expresses ...
 	DeleteTODOResponse struct{}
 )
+
+type TODO struct {
+	ID          int64     `json:"id"`
+	Subject     string    `json:"subject"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type CreateTODORequest struct {
+	Subject     string `json:"subject"`
+	Description string `json:"description"`
+}
+
+type UpdateTODORequest struct {
+	ID          int64  `json:"id"`
+	Subject     string `json:"subject"`
+	Description string `json:"description"`
+}
+
+type ReadTODORequest struct {
+	PrevID int64 `json:"prev_id"`
+	Size   int64 `json:"size"`
+}
+
+type DeleteTODORequest struct {
+	IDs []int64 `json:"ids"`
+}
